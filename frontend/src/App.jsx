20c7c644ea7c6d260fb2ae3f3412d4d62e2b6a04
sanki92 <sankalpt92@gmail.com>
2025-05-15 08:31:58 +0530

    feat: add protected routes for home, events, clubs, and profile pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,12 @@ import "./App.css";
 import SignUpScreen from "./components/SignUpScreen";
 import LoginScreen from "./components/LoginScreen";
 import VerifyEmail from "./pages/VerifyEmail";
+import Home from "./pages/Home";
+import SignInAccessibleLayout from "./components/common/SignInAccessibleLayout";
+import Events from "./pages/Events";
+import Clubs from "./pages/Clubs";
+import Profile from "./pages/Profile";
+import ProtectedRoute from "./services/ProtectedRoute";
 
 const App = () => {
   return (
@@ -16,9 +22,52 @@ const App = () => {
       <Routes>
         <Route path="/signup" element={<SignUpScreen />} />
         <Route path="/login" element={<LoginScreen />} />
-  
-        <Route path="/verify-email" element={<VerifyEmail/>} />
+
+        <Route path="/verify-email" element={<VerifyEmail />} />
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute allowedRoles={["student"]}>
+              <SignInAccessibleLayout>
+                <Home />
+              </SignInAccessibleLayout>
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/events"
+          element={
+            <ProtectedRoute allowedRoles={["student"]}>
+              <SignInAccessibleLayout>
+                <Events />
+              </SignInAccessibleLayout>
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/clubs"
+          element={
+            <ProtectedRoute allowedRoles={["student"]}>
+              <SignInAccessibleLayout>
+                <Clubs />
+              </SignInAccessibleLayout>
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute allowedRoles={["student"]}>
+              <SignInAccessibleLayout>
+                <Profile />
+              </SignInAccessibleLayout>
+            </ProtectedRoute>
+          }
+        />
+
         <Route path="/" element={<Navigate to="/signup" replace />} />
+
+        <Route path="/unauthorized" element={<h1>Unauthorized</h1>} />
         {/* <Route
           path="/student"
           element={
